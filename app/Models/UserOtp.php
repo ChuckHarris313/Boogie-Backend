7885194d380a7cf1231c1e8196683f568c66d232
Shahroz Malik <156915392+ChuckHarris313@users.noreply.github.com>
2024-01-17 18:17:57 +0500

    First-Commit
    First-Commit

---
+++ app/Models/UserOtp.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Twilio\Rest\Client;
+use Exception;
+class UserOtp extends Model
+{
+    use HasFactory;
+    protected $guarded = [''];
+    public function sendSMS($receiverNumber1)
+    {
+        $message = "Login OTP is ".$this->otp;
+        try {
+  
+            
+
+            $client = new Client($account_sid, $auth_token);
+            $client->messages->create($receiverNumber1, [
+                'from' => $twilio_number, 
+                'body' => $message
+            ]);
+            return true;
+    
+        } catch (Exception $e) {
+            return false;
+            return info("Error: ". $e->getMessage());
+        }
+    }
+}
